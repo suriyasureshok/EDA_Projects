89fbbc24d73b38d072176c2d2bff765c742510f8
suriyasureshok <240171.ad@rmkec.ac.in>
2024-12-07 19:39:17 +0530

    Update Chennai_Patients_analysis.py

--- Chennai_Patients_analysis.py
+++ Chennai_Patients_analysis.py
@@ -1,1 +1,9 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from collections import Counter 
+df = pd.read_csv("/kaggle/input/chennai-patients-data/Patient_Data_ChennaiCity.csv",encoding='ISO-8859-1')
+df
+df.isnull().sum()
 
